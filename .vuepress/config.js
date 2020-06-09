780e6360df8752c2270779b943e5d25c23495f72
i869639 <murali.varma@sap.com>
2020-06-08 20:10:26 -0400

    add nelify redirect config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
     nav: [
       { text: 'Github', link: 'https://github.com/muraliavarma' },
       { text: 'Linkedin', link: 'https://www.linkedin.com/in/murlax/' },
-      { text: 'Resume', link: 'Murali_Resume_2020_06.pdf' },
+      { text: 'Resume', link: 'https://murlax.com/resume' },
     ],
   },
 }
