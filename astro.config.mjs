91a2535dfc82b981d0a96d55701c343a9c1a0651
Murali <muraliavarma@gmail.com>
2025-12-09 20:59:59 -0500

    feat: Migrate project deployment from Netlify to Cloudflare.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://murlax.com',
+  site: "https://murlax.com",
   integrations: [vue()],
+
   markdown: {
     shikiConfig: {
-      theme: 'github-dark',
-      wrap: true
-    }
-  }
+      theme: "github-dark",
+      wrap: true,
+    },
+  },
+
+  adapter: cloudflare(),
+  output: "server",
 });
